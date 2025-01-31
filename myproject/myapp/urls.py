2c817d0818e415c524a333ebdc4ca6e786bd787d
maaz <maaz.shaikh@solytics-partners.com>
2025-02-01 00:30:27 +0530

    refact: updated method names and country to US

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -3,10 +3,5 @@ from myapp import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('news/<slug:news_category>/', views.news, name="news"),
-    # path('health/', views.health, name="health"),
-    # path('entertainment/', views.entertainment, name="entertainment"),
-    # path('business/', views.business, name="business"),
-    # path('technology/', views.technology, name="technology"),
-    # path('science/', views.science, name="science"),
+    path('news/<slug:news_category>/', views.read_news_with_category, name="news"),
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -2,25 +2,23 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 import requests
 import json
-from decouple import config
-# Create your views here.
 
 
 def home(request):
    
     return render(request, 'home.html')
 
-def news(request, news_category):
+def read_news_with_category(request, news_category):
     try:
-        context = {'sports_titles':conversion(news_category), 'category':news_category}
+        context = {'sports_titles':read_news(news_category), 'category':news_category}
         return render(request, 'news.html', context)
     except:
         print("There was a problem accessing the equipment data.")
         return HttpResponse("<h2>Server Issue</h2>")
 
-def conversion(news_category):
-    api_key = config('API_KEY')
-    url = f"https://newsapi.org/v2/top-headlines?country=in&category={news_category}&apiKey={api_key}"
+def read_news(news_category):
+    api_key = '509fadfddd8543bc89459c4b05f7c20d'
+    url = f"https://newsapi.org/v2/top-headlines?country=us&category={news_category}&apiKey={api_key}"
     news = requests.get(url).text
     news_dict = json.loads(news) # converting json data into Python object to access data
     articles = news_dict['articles']
